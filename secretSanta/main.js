45945b092e354720ccfc67218c7c058b2349d55f
Andrew Maxwell <andrew.maxwell@monsanto.com>
2020-11-20 15:34:19 -0600

    updated secret santa

--- secretSanta/main.js
+++ secretSanta/main.js
@@ -1,15 +1,7 @@
-let seed = 1;
-const rand = () => {
-  let t = (seed += 0x6d2b79f5);
-  t = Math.imul(t ^ (t >>> 15), t | 1);
-  t ^= t + Math.imul(t ^ (t >>> 7), t | 61);
-  return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
-};
-
 // shuffles an array using Fisher-Yates algorithm
 const shuffle = (arr) => {
   for (let counter = arr.length; counter; ) {
-    const index = Math.floor(rand() * counter--);
+    const index = Math.floor(Math.random() * counter--);
     [arr[counter], arr[index]] = [arr[index], arr[counter]];
   }
   return arr;
@@ -24,12 +16,13 @@ const getInitial = (people) => {
 // returns the index of the first person with a bad match, otherwise -1
 const getIndexOfBad = (result) =>
   result.findIndex(
-    ({name, exclude, match}) => name === match || exclude.includes(match)
+    ({name, exclude, match}) =>
+      name === match || exclude.includes(match.toUpperCase())
   );
 
 // swaps people between the person at indexOfBad and a random other person
 const swapWithRandom = (result, indexOfBad) => {
-  const index = Math.floor(rand() * result.length);
+  const index = Math.floor(Math.random() * result.length);
   const temp = result[indexOfBad].match;
   result[indexOfBad].match = result[index].match;
   result[index].match = temp;
@@ -45,40 +38,30 @@ const generateMatches = (people) => {
   }
 };
 
-const getOutput = (result, personIndex) => {
-  if (!result) return 'No solution found.';
+const go = () => {
+  const result = generateMatches(
+    window.input.value
+      .trim()
+      .split('\n')
+      .filter((r) => r.trim())
+      .map((r) => {
+        const [name, exclude] = r.split(/\s*-\s*/);
+        return {name, exclude: exclude.toUpperCase()};
+      })
+  );
 
-  if (result[personIndex]) {
-    const {name, match} = result[personIndex];
-    return `${name} will get a gift for ${match}`;
-  }
-
-  return result
+  window.output.innerHTML = result
     .map(
-      ({name}, i) =>
-        `<a href="${location.href.replace('_', i)}">Link for ${name}</a>`
+      ({name, match}) =>
+        `<a href="${location.href}#${btoa(match)}">Link for ${name}</a>`
     )
     .join('\n');
 };
 
-const go = () => {
-  const [[origSeed, personIndex], ...parts] = location.hash
-    .slice(1)
-    .split(';')
-    .map((p) => p.split(','));
+window.input.addEventListener('input', go);
 
-  seed = Number(origSeed);
-
-  const result = generateMatches(
-    parts.map(([name, ...x], i, arr) => ({
-      name,
-      exclude: x.map((i) => arr[i][0]),
-    }))
-  );
-
-  window.output.innerHTML = getOutput(result, personIndex);
-};
-
-go();
-
-// #1,3;Taylor,3,1;Jim,4,5;Emmett,3;Helen,0,6;Susan,1,7;Olivia,2;Dootman,0,7;PuddingGlove,4,6
+if (location.hash)
+  document.body.innerHTML = `You'll get a gift for ${atob(
+    location.hash.slice(1)
+  )}`;
+else go();
