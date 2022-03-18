22747c1289d06f750f0f9f9c87fda1a52a034e54
Andrew Maxwell <emailandrewmaxwell@gmail.com>
2022-03-18 09:36:16 -0500

    logarithmic scale

---
+++ logarithmicScale/main.js
@@ -0,0 +1,31 @@
+const pageLength = 2000;
+const textarea = document.querySelector('textarea');
+
+const doStuff = () => {
+  location.hash = encodeURIComponent(textarea.value.trim());
+  const data = textarea.value
+    .trim()
+    .split('\n')
+    .filter((l) => l.trim())
+    .map((r) => {
+      const [name, num] = r.trim().split(/\s*:\s*/);
+      return {name, size: parseFloat(num)};
+    });
+  data.sort((a, b) => a.size - b.size);
+  const min = Math.log(data[0].size);
+  const max = Math.log(data[data.length - 1].size);
+
+  let result = '';
+  for (const {name, size} of data) {
+    const y = ((Math.log(size) - min) / (max - min)) * pageLength;
+    result += `<div class="item" style="top: ${y}" title="${name}">${name}: ${size.toExponential()}m</div>`;
+  }
+
+  document.querySelector('#result').innerHTML = result;
+};
+
+if (location.hash.length > 1) {
+  textarea.value = decodeURIComponent(location.hash.slice(1));
+}
+textarea.addEventListener('input', doStuff);
+doStuff();
