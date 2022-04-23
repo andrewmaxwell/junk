628ac40d3283c50a041cc9360c51f9b2017e017a
Andrew Maxwell <emailandrewmaxwell@gmail.com>
2022-04-22 19:03:23 -0500

    update eslint, delete dev-team directory

--- dev-team/.eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  globals: Object.keys(require('ramda')).reduce((acc, key) => {
-    acc[key] = 'readonly'
-    return acc;
-  }, {})
-};
--- dev-team/constants.js
+++
@@ -1,6 +0,0 @@
-export const columns = ['analyze', 'dev', 'test', 'done'];
-
-export const initialState = {
-  tasks: [{id: 'First Task', estimate: 10, column: 'analyze'}],
-  people: [{id: 'Founder', analyze: 1, dev: 1, test: 1}]
-};
