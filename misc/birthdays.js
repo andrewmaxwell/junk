aeb5df78a4b251bd139321ba876e80fb4685fb2e
Andrew Maxwell <emailandrewmaxwell@gmail.com>
2023-10-28 17:00:28 -0500

    Update birthdays.js

--- misc/birthdays.js
+++ misc/birthdays.js
@@ -1,41 +1,39 @@
-const data = `Matt		1973/07/08
-Krys	1	1983/12/30
+const data = `
+Krys		1983/12/30
 Josh G		1987/12/21
-Andrew		1988/06/30
-Megan	1	1988/09/13
+Andrew		1988-06-30
 Manny	1	1989/10/20
-Jenna	1	1990/01/25
-Caleb	1	2005/02/09
-Mirielle		2005/03/20
+Jenna		1990/01/25
 Abraham	1	2005/10/19
 Ayden	1	2005/11/14
 Peter		2006/02/17
 Polina		2006/04/05
-Jaisaac	1	2007/01/23
 Illia		2007/05/20
 Casey		2007/06/13
-Sarah	1	2007/07/11
+Sarah		2007/07/11
 Daniel T	1	2007/08/06
 Claire	1	2007/09/08
 Hannah		2007/10/13
 Loralee		2007/12/22
 Jadon		2008/04/26
-Jeremiah	1	2008/06/22
+Eliza		2008/09/24
 Levi		2008/11/28
-Kaden		2008/12/13
+Kaden	1	2008/12/13
+Nick		2009/03/27
 Anneliese		2009/11/18
 Morgan		2010/02/03
 Victoria	1	2010/02/28
 Brady		2010/03/20
-Makayla 	1	2010/10/15
-Hope		2011/01/20
-Bella		2011/05/12`
+Susan		2010/05/26
+Noelle		2011/06/20
+Calvin		2011/12/23`
+  .trim()
   .split('\n')
   .map((r) => {
     const [name, , date] = r.split('\t');
     return {name, date: Date.parse(date)};
   })
-  .sort((a, b) => a.date - b.date);
+  .sort((a, b) => a.twice - b.twice);
 
 const result = [];
 for (let i = 0; i < data.length - 1; i++) {
@@ -43,20 +41,20 @@ for (let i = 0; i < data.length - 1; i++) {
     result.push({
       a: data[i].name,
       b: data[j].name,
-      date: new Date(2 * data[j].date - data[i].date),
+      twice: new Date(2 * data[j].date - data[i].date),
     });
   }
 }
 
 console.log(
   result
-    .sort((a, b) => a.date - b.date)
+    .sort((a, b) => a.twice - b.twice)
     // .filter(({a, b}) => (a + b).includes('Andrew'))
     .map(
-      ({a, b, date}) =>
+      ({a, b, twice}) =>
         `${a} ${
-          date < Date.now() ? 'was' : 'will be'
-        } twice as old as ${b} on ${date.toDateString()}`
+          twice < Date.now() ? 'was' : 'will be'
+        } twice as old as ${b} on ${twice.toDateString()}`
     )
     .join('\n')
 );
@@ -69,7 +67,5 @@ for (let i = 0; i < data.length - 1; i++) {
     Math.round(days),
     'days older than',
     data[i + 1].name
-    // '\n.'.repeat(days / 30.5)
   );
 }
-console.log('Bella is the youngest.');
