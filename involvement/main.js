60bb3fb66c5bd89bb991390f1f63541d61b6733c
Andrew Maxwell <emailandrewmaxwell@gmail.com>
2023-12-07 20:19:18 -0600

    involvement search

--- involvement/main.js
+++ involvement/main.js
@@ -1,48 +1,47 @@
-const {XLSX} = window;
-const url =
-  'https://docs.google.com/spreadsheets/d/e/2PACX-1vRNj5GTusTsfQlo7FcTFieaD_x_bhdtKKp9gbfOoyX9q9Kde3UrYI-TehtpwkXfee9MVRI6848NzcO6/pub?output=xlsx';
+const getRows = async () => {
+  const {XLSX} = window;
+  const url =
+    'https://docs.google.com/spreadsheets/d/e/2PACX-1vRNj5GTusTsfQlo7FcTFieaD_x_bhdtKKp9gbfOoyX9q9Kde3UrYI-TehtpwkXfee9MVRI6848NzcO6/pub?output=xlsx';
+  const data = XLSX.read(await (await fetch(url)).arrayBuffer(), {
+    cellDates: true,
+  }).Sheets;
 
-const data = XLSX.read(await (await fetch(url)).arrayBuffer(), {
-  cellDates: true,
-}).Sheets;
+  for (const sheet in data) {
+    data[sheet] = XLSX.utils.sheet_to_json(data[sheet]);
+  }
 
-for (const sheet in data) {
-  data[sheet] = XLSX.utils.sheet_to_json(data[sheet]);
-}
+  const peopleIndex = Object.fromEntries(
+    [...data.Members, ...data.Ministries, ...data['Non-members']].map(
+      ({Name}) => [Name, {ministries: []}]
+    )
+  );
+  for (const {Name, Ministry} of data.Ministries) {
+    peopleIndex[Name].ministries.push(Ministry);
+  }
+  for (const {Name, 'Member Since': memberSince} of data.Members) {
+    peopleIndex[Name].memberSince = memberSince.toISOString().slice(0, 10);
+  }
 
-const peopleIndex = {};
-const getPerson = (personName) =>
-  (peopleIndex[personName] = peopleIndex[personName] || {ministries: []});
+  return Object.entries(peopleIndex)
+    .map(([name, {ministries, memberSince = ''}]) => [
+      name,
+      memberSince,
+      ministries.sort().join(', '),
+    ])
+    .sort();
+};
 
-for (const {Name, Ministry} of data.Ministries) {
-  getPerson(Name).ministries.push(Ministry);
-}
-for (const {Name, 'Member Since': memberSince} of data.Members) {
-  getPerson(Name).memberSince = memberSince.toISOString().slice(0, 10);
-}
-for (const {Name} of data['Non-members']) {
-  getPerson(Name);
-}
+const rows = await getRows();
 
-const rows = Object.entries(peopleIndex)
-  .map(([name, {ministries, memberSince = ''}]) => [
-    name,
-    memberSince,
-    ministries.sort().join(', '),
-  ])
-  .sort();
+const render = (searchValue) => {
+  window.tbody.innerHTML = rows
+    .filter((row) => row.join('|').toUpperCase().includes(searchValue))
+    .map((row) => `<tr>${row.map((v) => `<td>${v}</td>`).join('')}</tr>`)
+    .join('');
+};
 
-document.querySelector('#result').innerHTML = `<table>
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Member Since</th>
-        <th>Ministries</th>
-      </tr>
-    </thead>
-    <tbody>
-      ${rows
-        .map((row) => `<tr>${row.map((v) => `<td>${v}</td>`).join('')}</tr>`)
-        .join('')}
-    </tbody>
-  </table>`;
+window.search.addEventListener('input', (e) => {
+  render(e.target.value.toUpperCase());
+});
+
+render('');
